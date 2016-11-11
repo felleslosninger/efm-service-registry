824a87b9dccf63919a44e9eeaf7f6b31defe11c4
Martin <martin.wam@difi.no>
2016-11-11 14:54:39 +0100

    MOVE-64: QA.

--- serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/servicerecord/ServiceRecordFactory.java
+++ serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/servicerecord/ServiceRecordFactory.java
@@ -1,6 +1,5 @@
 package no.difi.meldingsutveksling.serviceregistry.servicerecord;
 
-import java.security.cert.Certificate;
 import no.difi.meldingsutveksling.ptp.KontaktInfo;
 import no.difi.meldingsutveksling.serviceregistry.CertificateNotFoundException;
 import no.difi.meldingsutveksling.serviceregistry.config.ServiceregistryProperties;
@@ -12,10 +11,16 @@ import no.difi.meldingsutveksling.serviceregistry.service.virksert.CertificateTo
 import no.difi.meldingsutveksling.serviceregistry.service.virksert.VirkSertService;
 import no.difi.vefa.peppol.common.model.Endpoint;
 import no.difi.virksert.client.VirksertClientException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+import java.security.cert.Certificate;
 
 /**
  * Factory method class to create Service Records based on lookup endpoint urls and certificates corresponding to those services
  */
+@Component
 public class ServiceRecordFactory {
 
     private final KrrService krrService;
@@ -34,6 +39,7 @@ public class ServiceRecordFactory {
      * @param ksLookup - used to lookup if ks should be used for transportation
      * @param krrService - used to lookup parameters needed to use DPI transportation
      */
+    @Autowired
     public ServiceRecordFactory(ServiceregistryProperties properties, VirkSertService virksertService, ELMALookupService elmaLookupService, KSLookup ksLookup, KrrService krrService) {
         this.properties = properties;
         this.virksertService = virksertService;
@@ -42,6 +48,7 @@ public class ServiceRecordFactory {
         this.krrService = krrService;
     }
 
+    @PreAuthorize("#oauth2.hasScope('move/dpo.read')")
     public ServiceRecord createEduServiceRecord(String orgnr) {
         String finalOrgNumber = ksLookup.mapOrganizationNumber(orgnr);
         Endpoint ep = elmaLookupService.lookup(NORWAY_PREFIX + finalOrgNumber);
@@ -49,6 +56,7 @@ public class ServiceRecordFactory {
         return new EDUServiceRecord(properties, pemCertificate, ep.getAddress(), orgnr);
     }
 
+    @PreAuthorize("#oauth2.hasScope('move/dpv.read')")
     public ServiceRecord createPostVirksomhetServiceRecord(String orgnr) {
         return new PostVirksomhetServiceRecord(properties, orgnr);
     }
@@ -62,6 +70,7 @@ public class ServiceRecordFactory {
         }
     }
 
+    @PreAuthorize("#oauth2.hasScope('move/dpi.read')")
     public ServiceRecord createSikkerDigitalPostRecord(String identifier) {
         final KontaktInfo kontaktInfo = krrService.getCitizenInfo(identifier);
         return new SikkerDigitalPostServiceRecord(properties, kontaktInfo, ServiceIdentifier.DPI, identifier);
