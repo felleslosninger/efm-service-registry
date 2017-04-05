d8ba9edaafddd1c1f9a99d6893e430ad8d542b2f
Martin <martin.wam@difi.no>
2017-04-05 14:22:58 +0200

    MOVE-189: Moved to move-common cloud.

--- serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/MoveServiceRegistryApplication.java
+++ serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/MoveServiceRegistryApplication.java
@@ -1,7 +1,7 @@
 package no.difi.meldingsutveksling.serviceregistry;
 
 import no.difi.meldingsutveksling.serviceregistry.config.ServiceregistryProperties;
-import no.difi.meldingsutveksling.serviceregistry.spring.cloud.SpringCloudProtocolResolver;
+import no.difi.move.common.config.SpringCloudProtocolResolver;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
