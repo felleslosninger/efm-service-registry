0dc065cfca6ecc47c19ff1a54b5a73134ae62b92
Martin <martin.wam@difi.no>
2017-06-20 10:22:44 +0200

    MOVE-237: Removed unused methods.

--- serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/controller/SasTokenController.java
+++ serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/controller/SasTokenController.java
@@ -14,14 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-
 @RestController
 public class SasTokenController {
 
@@ -53,47 +45,4 @@ public class SasTokenController {
         return ResponseEntity.ok(payloadSigner.sign(sasToken));
     }
 
-    private String genSASToken(String resourceUri, String keyName, String key) {
-
-        long epoch = System.currentTimeMillis() / 1000L;
-        String expirySeconds = Long.toString(epoch + 60 * 3);
-
-        String sasToken = null;
-        try {
-            String stringToSign = String.format("%s\n%s", URLEncoder.encode(resourceUri, "UTF-8"), expirySeconds);
-            String signature = getHMAC256(key, stringToSign);
-            sasToken = String.format("SharedAccessSignature sr=%s&sig=%s&se=%s&skn%s",
-                    URLEncoder.encode(resourceUri, "UTF-8"),
-                    URLEncoder.encode(signature, "UTF-8"),
-                    expirySeconds, keyName);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
-        return sasToken;
-    }
-
-    private String getHMAC256(String key, String input) {
-        Mac sha256_HMAC = null;
-        String hash = null;
-        try {
-            sha256_HMAC = Mac.getInstance("HmacSHA256");
-            SecretKeySpec secret_key = new SecretKeySpec(key.getBytes(), "HmacSHA256");
-            sha256_HMAC.init(secret_key);
-            Base64.Encoder encoder = Base64.getEncoder();
-
-            hash = new String(encoder.encode(sha256_HMAC.doFinal(input.getBytes("UTF-8"))));
-
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
-        return hash;
-    }
 }
