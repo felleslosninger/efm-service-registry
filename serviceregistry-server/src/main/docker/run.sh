0d756868f7577b4d696ef1f70401e520adc884e9
mwa <martin.wam@difi.no>
2018-03-14 12:50:52 +0100

    removing dockerize from run.sh

--- serviceregistry-server/src/main/docker/run.sh
+++ serviceregistry-server/src/main/docker/run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-dockerize -wait http://eureka:8761/discovery/manage/health -wait tcp://logstash:8300 && \
-    java -jar ${APP_JAVA_PARAMS} ${APP_DIR}/app.jar --spring.profiles.active=${APP_PROFILE} "$@"
+exec java -jar ${APP_JAVA_PARAMS} ${APP_DIR}/app.jar --spring.profiles.active=${APP_PROFILE} "$@"
