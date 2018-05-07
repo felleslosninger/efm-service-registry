de2f9bdb5e122fb4ffb8328e0b164844c58259a4
maw <maw@esito.no>
2018-05-07 15:45:26 +0200

    MOVE-719: Workaround in case of SvarUt errors

--- svarut-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/svarut/SvarUtService.java
+++ svarut-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/svarut/SvarUtService.java
@@ -1,9 +1,11 @@
 package no.difi.meldingsutveksling.serviceregistry.svarut;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SvarUtService {
 
     private SvarUtClient svarUtClient;
@@ -15,7 +17,12 @@ public class SvarUtService {
 
     public boolean hasSvarUtAdressering(String orgnr) {
         RetrieveMottakerSystemForOrgnr request = RetrieveMottakerSystemForOrgnr.builder().withOrganisasjonsnr(orgnr).build();
-        RetrieveMottakerSystemForOrgnrResponse response = svarUtClient.retrieveMottakerSystemForOrgnr(request);
-        return !response.getReturn().isEmpty();
+        try {
+            RetrieveMottakerSystemForOrgnrResponse response = svarUtClient.retrieveMottakerSystemForOrgnr(request);
+            return !response.getReturn().isEmpty();
+        } catch (Exception e) {
+            log.error("Caught exception while looking up receiver in SvarUt", e);
+            return false;
+        }
     }
 }
