bc139ec8068259cc3295c6d43df283149da0b98a
Johannes Molland <johannes.molland@difi.no>
2019-08-16 12:06:40 +0200

    Configures spring security session management.

--- serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/config/WebSecurityConfig.java
+++ serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +30,9 @@ public class WebSecurityConfig {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.headers().frameOptions().sameOrigin();
-            http.csrf().disable()
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .csrf().disable()
                     .authorizeRequests()
                     .antMatchers("/h2-console/**").permitAll()
                     .antMatchers("/api/**").authenticated()
