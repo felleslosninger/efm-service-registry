e5be97d460704ab0eadbc403c058e16ff39a0888
Martin Wam <martin.wam@gmail.com>
2021-02-23 10:59:36 +0100

    MOVE-2305: Multi tenant oauth

--- common/src/main/java/no/difi/meldingsutveksling/serviceregistry/config/ServiceregistryProperties.java
+++ common/src/main/java/no/difi/meldingsutveksling/serviceregistry/config/ServiceregistryProperties.java
@@ -56,6 +56,8 @@ public class ServiceregistryProperties {
     @Data
     public static class Auth {
         private String sasToken;
+        private String oidcIssuer;
+        private String maskinportenIssuer;
     }
 
     @Data
