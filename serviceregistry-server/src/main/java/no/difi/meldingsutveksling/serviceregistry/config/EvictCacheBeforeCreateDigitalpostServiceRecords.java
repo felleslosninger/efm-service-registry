83d63e053deb9051b33bca4973391b5b914dcd3a
Steinar Henriksen <steinar.henriksen@digdir.no>
2021-05-28 16:50:38 +0200

    MOVE-2440 Added aspect for profile yt causing cache invalidation before each created digitalpost service record

---
+++ serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/config/EvictCacheBeforeCreateDigitalpostServiceRecords.java
@@ -0,0 +1,42 @@
+package no.difi.meldingsutveksling.serviceregistry.config;
+
+import lombok.RequiredArgsConstructor;
+import no.difi.meldingsutveksling.serviceregistry.CacheConfig;
+import no.difi.meldingsutveksling.serviceregistry.SRRequestScope;
+import no.difi.meldingsutveksling.serviceregistry.krr.LookupParameters;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableAspectJAutoProxy // Enables defining aspects using @Aspect annotations
+@Profile("yt")
+@Aspect
+@RequiredArgsConstructor
+public class EvictCacheBeforeCreateDigitalpostServiceRecords {
+
+    private final SRRequestScope requestScope;
+    private final CacheManager cacheManager;
+
+    @Before("execution(* no.difi.meldingsutveksling.serviceregistry.record.ServiceRecordService.createDigitalpostServiceRecords(..)) && args(identifier, ..)")
+    public void evictCacheBeforeCreateDigitalpostServiceRecords(String identifier) {
+        LookupParameters lookupParameters = LookupParameters
+                .lookup(identifier)
+                .token(requestScope.getToken());
+
+        Cache krrCache = cacheManager.getCache(CacheConfig.KRR_CACHE);
+        if (krrCache != null) {
+            krrCache.evictIfPresent(lookupParameters);
+        }
+
+        Cache dsfCache = cacheManager.getCache(CacheConfig.DSF_CACHE);
+        if (dsfCache != null) {
+            dsfCache.evictIfPresent(lookupParameters);
+        }
+    }
+
+}
