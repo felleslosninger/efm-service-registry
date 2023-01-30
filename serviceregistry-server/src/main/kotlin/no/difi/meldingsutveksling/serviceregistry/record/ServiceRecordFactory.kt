f7b6087241a05020973167ecd2d5b86490f15e16
Lars Bardgard Astveit <lab@digdir.no>
2023-01-30 13:33:11 +0100

    Refactor and remove old comments

--- serviceregistry-server/src/main/kotlin/no/difi/meldingsutveksling/serviceregistry/record/ServiceRecordFactory.kt
+++ serviceregistry-server/src/main/kotlin/no/difi/meldingsutveksling/serviceregistry/record/ServiceRecordFactory.kt
@@ -28,13 +28,15 @@ import java.nio.charset.StandardCharsets
 import java.util.*
 
 @Component
-class ServiceRecordFactory(private val properties: ServiceregistryProperties,
-                           private val virkSertService: VirkSertService,
-                           private val processService: ProcessService,
-                           private val documentTypeService: DocumentTypeService,
-                           private val kontaktInfoService: KontaktInfoService,
-                           private val entityService: EntityService,
-                           private val requestScope: SRRequestScope) {
+class ServiceRecordFactory(
+    private val properties: ServiceregistryProperties,
+    private val virkSertService: VirkSertService,
+    private val processService: ProcessService,
+    private val documentTypeService: DocumentTypeService,
+    private val kontaktInfoService: KontaktInfoService,
+    private val entityService: EntityService,
+    private val requestScope: SRRequestScope
+) {
 
     val log = logger()
 
@@ -48,89 +50,121 @@ class ServiceRecordFactory(private val properties: ServiceregistryProperties,
     }
 
     fun createDpfServiceRecord(orgnr: String, process: Process, securityLevel: Int): ServiceRecord {
-        val serviceRecord = ServiceRecord(ServiceIdentifier.DPF, orgnr, process, properties.fiks.svarut.serviceRecordUrl.toString())
+        val serviceRecord =
+            ServiceRecord(ServiceIdentifier.DPF, orgnr, process, properties.fiks.svarut.serviceRecordUrl.toString())
         serviceRecord.pemCertificate = try {
             IOUtils.toString(properties.fiks.svarut.certificate.inputStream, StandardCharsets.UTF_8)
         } catch (e: IOException) {
-            log.error(SRMarkerFactory.markerFrom(requestScope), "Could not read certificate from {}", properties.fiks.svarut.certificate.toString())
+            log.error(
+                SRMarkerFactory.markerFrom(requestScope),
+                "Could not read certificate from {}",
+                properties.fiks.svarut.certificate.toString()
+            )
             throw ServiceRegistryException(e)
         }
         serviceRecord.service.securityLevel = securityLevel
         return serviceRecord
     }
 
-    fun createDigitalServiceRecord(personResource: PersonResource, identifier: String, process: Process): ServiceRecord {
-        val serviceRecord = SikkerDigitalPostServiceRecord(identifier, process, personResource, properties.dpi.endpointURL.toString(), false, null, null)
+    fun createDigitalServiceRecord(
+        personResource: PersonResource,
+        identifier: String,
+        process: Process
+    ): ServiceRecord {
+        val serviceRecord = SikkerDigitalPostServiceRecord(
+            identifier,
+            process,
+            personResource,
+            properties.dpi.endpointURL.toString(),
+            false,
+            null,
+            null
+        )
         documentTypeService.findByBusinessMessageType(BusinessMessageTypes.DIGITAL)
-                .orElseThrow { missingDocTypeException(BusinessMessageTypes.DIGITAL) }
-                .let { serviceRecord.documentTypes = listOf(it.identifier) }
+            .orElseThrow { missingDocTypeException(BusinessMessageTypes.DIGITAL) }
+            .let { serviceRecord.documentTypes = listOf(it.identifier) }
         return serviceRecord
     }
 
     fun createDigitalDpvServiceRecord(identifier: String, process: Process): ServiceRecord {
-        val dpvServiceRecord = ServiceRecord(ServiceIdentifier.DPV, identifier, process, properties.dpv.endpointURL.toString())
+        val dpvServiceRecord =
+            ServiceRecord(ServiceIdentifier.DPV, identifier, process, properties.dpv.endpointURL.toString())
         val defaultArkivmeldingProcess = processService.defaultArkivmeldingProcess
         dpvServiceRecord.service.serviceCode = defaultArkivmeldingProcess.serviceCode
         dpvServiceRecord.service.serviceEditionCode = defaultArkivmeldingProcess.serviceEditionCode
         documentTypeService.findByBusinessMessageType(BusinessMessageTypes.DIGITAL_DPV)
-                .orElseThrow { missingDocTypeException(BusinessMessageTypes.DIGITAL_DPV) }
-                .let { dpvServiceRecord.documentTypes = listOf(it.identifier) }
+            .orElseThrow { missingDocTypeException(BusinessMessageTypes.DIGITAL_DPV) }
+            .let { dpvServiceRecord.documentTypes = listOf(it.identifier) }
         return dpvServiceRecord
     }
 
     fun createFullname(fregGatewayEntity: FregGatewayEntity.Address.Response): String {
         val firstname = fregGatewayEntity.navn.fornavn.plus(" ")
-        val middle = if (!fregGatewayEntity.navn.mellomnavn.isNullOrEmpty()) fregGatewayEntity.navn.mellomnavn.plus(" ") else ""
+        val middle =
+            if (!fregGatewayEntity.navn.mellomnavn.isNullOrEmpty()) fregGatewayEntity.navn.mellomnavn.plus(" ") else ""
         val surname = fregGatewayEntity.navn.etternavn
 
         return firstname.plus(middle).plus(surname);
     }
 
     @Throws(FregGatewayException::class, BrregNotFoundException::class)
-    fun createPrintServiceRecord(identifier: String,
-                                 onBehalfOrgnr: String,
-                                 token: Jwt,
-                                 personResource: PersonResource,
-                                 p: Process,
-                                 print: Boolean): Optional<ServiceRecord> {
-        if(!print) {
+    fun createPrintServiceRecord(
+        identifier: String,
+        onBehalfOrgnr: String,
+        token: Jwt,
+        personResource: PersonResource,
+        p: Process,
+        print: Boolean
+    ): Optional<ServiceRecord> {
+        if (!print) {
             //To allow SR to avoid DSF-lookup sending print=false as a @RequestParam to improve performance.
-           return Optional.empty()
+            return Optional.empty()
         }
         kontaktInfoService.setPrintDetails(personResource)
 
-       val fregGatewayEntity = kontaktInfoService.getFregAdress(LookupParameters.lookup(identifier))
-           .orElseThrow { FregGatewayException("Receiver found in KRR on behalf of '$onBehalfOrgnr', but not in FREG.") }
-        if(Objects.isNull(fregGatewayEntity.postadresse)){
+        val fregGatewayEntity = kontaktInfoService.getFregAdress(LookupParameters.lookup(identifier))
+            .orElseThrow { FregGatewayException("Receiver found in KRR on behalf of '$onBehalfOrgnr', but not in FREG.") }
+        if (Objects.isNull(fregGatewayEntity.postadresse)) {
             // Some receivers have secret address - skip
             return Optional.empty();
         }
 
         val name = createFullname(fregGatewayEntity)
         val addressline = fregGatewayEntity.postadresse.adresselinje.joinToString(separator = " ")
-        val postAddress = PostAddress(name,
+        val postAddress = PostAddress(
+            name,
             addressline,
             fregGatewayEntity.postadresse.postnummer,
             fregGatewayEntity.postadresse.poststed,
             fregGatewayEntity.postadresse.landkode
-            )
+        )
         val senderEntity: Optional<EntityInfo> = entityService.getEntityInfo(onBehalfOrgnr)
 
-        val returnAddress = if (senderEntity.isPresent && senderEntity.get() is OrganizationInfo) {
-            val orginfo = senderEntity.get() as OrganizationInfo
-            PostAddress(orginfo.organizationName,
+        val returnAddress =
+            if (senderEntity.isPresent && senderEntity.get() is OrganizationInfo) {
+                val orginfo = senderEntity.get() as OrganizationInfo
+                PostAddress(
+                    orginfo.organizationName,
                     orginfo.postadresse.adresse,
                     orginfo.postadresse.postnummer,
                     orginfo.postadresse.poststed,
-                    orginfo.postadresse.land)
+                    orginfo.postadresse.land
+                )
             } else {
-                throw BrregNotFoundException(String.format("Sender with identifier=%s not found in BRREG", onBehalfOrgnr))
+                throw BrregNotFoundException(
+                    String.format(
+                        "Sender with identifier=%s not found in BRREG",
+                        onBehalfOrgnr
+                    )
+                )
             }
-        val printRecord = SikkerDigitalPostServiceRecord(identifier, p, personResource,
-                properties.dpi.endpointURL.toString(), true, postAddress, returnAddress)
+        val printRecord = SikkerDigitalPostServiceRecord(
+            identifier, p, personResource,
+            properties.dpi.endpointURL.toString(), true, postAddress, returnAddress
+        )
         documentTypeService.findByBusinessMessageType(BusinessMessageTypes.PRINT)
-                .orElseThrow { missingDocTypeException(BusinessMessageTypes.PRINT) }
-                .let { printRecord.documentTypes = listOf(it.identifier) }
+            .orElseThrow { missingDocTypeException(BusinessMessageTypes.PRINT) }
+            .let { printRecord.documentTypes = listOf(it.identifier) }
         return Optional.of(printRecord)
     }
 
@@ -142,7 +176,8 @@ class ServiceRecordFactory(private val properties: ServiceregistryProperties,
 
 
     fun createDpvServiceRecord(orgnr: String, process: Process): ServiceRecord {
-        val dpvServiceRecord = ServiceRecord(ServiceIdentifier.DPV, orgnr, process, properties.dpv.endpointURL.toString())
+        val dpvServiceRecord =
+            ServiceRecord(ServiceIdentifier.DPV, orgnr, process, properties.dpv.endpointURL.toString())
         dpvServiceRecord.service.serviceCode = process.serviceCode
         dpvServiceRecord.service.serviceEditionCode = process.serviceEditionCode
         return dpvServiceRecord
