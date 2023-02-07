2955da098494b63a2226851bb6827b145edb4ad3
Lars Bardgard Astveit <lab@digdir.no>
2023-02-07 14:35:07 +0100

    MOVE-3123 Fix mock for freg-client: Returns Raff Raffen dummy user when user does not exist in FREG. Returns user if they exist in FREG

--- freg-gateway-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/freg/mock/DefaultFregClientWithMock.java
+++ freg-gateway-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/freg/mock/DefaultFregClientWithMock.java
@@ -4,11 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import no.difi.meldingsutveksling.serviceregistry.config.ServiceregistryProperties;
 import no.difi.meldingsutveksling.serviceregistry.freg.client.DefaultFregGatewayClient;
 import no.difi.meldingsutveksling.serviceregistry.freg.domain.FregGatewayEntity;
-import no.difi.meldingsutveksling.serviceregistry.freg.exception.NotFoundInMfGatewayException;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -22,21 +22,22 @@ public class DefaultFregClientWithMock extends DefaultFregGatewayClient {
 
     @Override
     public Optional<FregGatewayEntity.Address.Response> getPersonAdress(String pid) {
-        ArrayList<String> adresselinje = new ArrayList<>();
-        adresselinje.add("Portveien 2");
-        return super.getPersonAdress(pid)
-                .or(() -> Optional.of(FregGatewayEntity.Address.Response.builder()
+        try {
+            return super.getPersonAdress(pid);
+        } catch (Exception e) {
+            return Optional.of(FregGatewayEntity.Address.Response.builder()
                     .personIdentifikator(pid)
                     .navn(FregGatewayEntity.Address.Navn.builder()
                             .fornavn("Raff")
                             .etternavn("Raffen")
                             .build())
                     .postadresse(FregGatewayEntity.Address.PostAdresse.builder()
-                            .adresselinje(adresselinje)
+                            .adresselinje(new ArrayList<>(List.of("Portveien 2")))
                             .postnummer("0468")
                             .poststed("Oslo")
                             .landkode("Norge")
                             .build())
-                    .build()));
+                .build());
+        }
     }
 }
