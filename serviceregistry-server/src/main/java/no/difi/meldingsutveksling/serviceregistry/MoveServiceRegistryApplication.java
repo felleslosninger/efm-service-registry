f5ebe32a5c240cd53cd6bac12522d416451baf6f
Frode Johansen <frode.johansen@digdir.no>
2023-11-14 12:32:09 +0100

    MOVE-3649: fjerner 'cloud' oppsett

--- serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/MoveServiceRegistryApplication.java
+++ serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/MoveServiceRegistryApplication.java
@@ -1,10 +1,8 @@
 package no.difi.meldingsutveksling.serviceregistry;
 
 import no.difi.meldingsutveksling.serviceregistry.config.ServiceregistryProperties;
-import no.difi.move.common.config.SpringCloudProtocolResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
