245412ce6f5751352af6654ae5387ccdf315f8b4
Frode Johansen <frode.johansen@digdir.no>
2023-11-14 15:21:25 +0100

    MOVE-3649: custom 'healthCheckHandler'

--- serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/MoveServiceRegistryApplication.java
+++ serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/MoveServiceRegistryApplication.java
@@ -1,11 +1,14 @@
 package no.difi.meldingsutveksling.serviceregistry;
 
+import com.netflix.appinfo.HealthCheckHandler;
+import com.netflix.appinfo.InstanceInfo;
 import no.difi.meldingsutveksling.serviceregistry.config.ServiceregistryProperties;
 import no.difi.move.common.config.SpringCloudProtocolResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ServiceregistryProperties.class})
@@ -15,5 +18,15 @@ public class MoveServiceRegistryApplication {
         new SpringApplicationBuilder(MoveServiceRegistryApplication.class)
                 .initializers(new SpringCloudProtocolResolver())
                 .run(args);
+    }
+
+    @Bean
+    public HealthCheckHandler healthCheckHandler() {
+        return new HealthCheckHandler() {
+            @Override
+            public InstanceInfo.InstanceStatus getStatus(InstanceInfo.InstanceStatus currentStatus) {
+                return InstanceInfo.InstanceStatus.UP;
+            }
+        };
     }
 }
