cfa59e613baaecd30fca1de0f4eb3647196dfce6
Steinar Henriksen <steinar.henriksen@digdir.no>
2024-03-20 13:16:37 +0100

    Fjerna bruk av Eureka

--- serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/MoveServiceRegistryApplication.java
+++ serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/MoveServiceRegistryApplication.java
@@ -1,16 +1,9 @@
 package no.difi.meldingsutveksling.serviceregistry;
 
-import com.netflix.appinfo.HealthCheckHandler;
-import com.netflix.appinfo.InstanceInfo;
 import no.difi.meldingsutveksling.serviceregistry.config.ServiceregistryProperties;
-import no.difi.move.common.config.SpringCloudProtocolResolver;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ServiceregistryProperties.class})
@@ -19,7 +12,6 @@ public class MoveServiceRegistryApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(MoveServiceRegistryApplication.class)
-                .initializers(new SpringCloudProtocolResolver())
                 .run(args);
     }
 
