dfdb225154eb35b0ae42c5cbc77684a84f8029bf
Johannes Molland <12180816+johannmo@users.noreply.github.com>
2024-07-23 22:22:06 +0200

    refactor: Remove unnecessary imports

--- freg-gateway-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/freg/client/DefaultFregGatewayClient.java
+++ freg-gateway-client/src/main/java/no/difi/meldingsutveksling/serviceregistry/freg/client/DefaultFregGatewayClient.java
@@ -1,18 +1,14 @@
 package no.difi.meldingsutveksling.serviceregistry.freg.client;
 
+import lombok.RequiredArgsConstructor;
 import no.difi.meldingsutveksling.serviceregistry.config.ServiceregistryProperties;
-import no.difi.meldingsutveksling.serviceregistry.freg.exception.NotFoundInMfGatewayException;
+import no.difi.meldingsutveksling.serviceregistry.freg.domain.FregGatewayEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-
-import no.difi.meldingsutveksling.serviceregistry.freg.domain.FregGatewayEntity;
 
 import java.util.Optional;
 
