2d7c19582eb072c501195548dc648c543acfb536
joachimhagheim <47362824+joachimhagheim@users.noreply.github.com>
2025-03-21 10:38:42 +0100

    MOVE-4327 Refactor VirksertConfig to be much shorter
    It also no longer gives you MOVE as default mode. This is done in properties instead.

--- serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/config/VirksertConfig.java
+++ serviceregistry-server/src/main/java/no/difi/meldingsutveksling/serviceregistry/config/VirksertConfig.java
@@ -2,12 +2,10 @@ package no.difi.meldingsutveksling.serviceregistry.config;
 
 import lombok.RequiredArgsConstructor;
 import no.difi.meldingsutveksling.serviceregistry.exceptions.ServiceRegistryException;
-import no.difi.virksert.api.Mode;
 import no.difi.virksert.client.BusinessCertificateClient;
 import no.difi.virksert.lang.BusinessCertificateException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -19,35 +17,11 @@ public class VirksertConfig {
 
     @Bean
     public BusinessCertificateClient virksertClient() {
-        BusinessCertificateClient virksertClient;
-
-        URI virksertUrl;
         try {
-            virksertUrl = properties.getVirksert().getEndpointURL().toURI();
-        } catch (URISyntaxException e) {
+            URI virksertUrl = properties.getVirksert().getEndpointURL().toURI();
+            return BusinessCertificateClient.of(virksertUrl, properties.getVirksert().getMode());
+        } catch (URISyntaxException | BusinessCertificateException e) {
             throw new ServiceRegistryException(e);
         }
-
-        try {
-            if (properties.getVirksert().getMode().equalsIgnoreCase("prod")) {
-                virksertClient = BusinessCertificateClient.of(virksertUrl, Mode.PRODUCTION);
-            } else if (properties.getVirksert().getMode().equalsIgnoreCase("move")) {
-                virksertClient = BusinessCertificateClient.of(virksertUrl, Mode.MOVE);
-
-              // Are these needed? Think they are only used by Virksert itself?
-            } else if (properties.getVirksert().getMode().equalsIgnoreCase("codecept")) {
-                virksertClient = BusinessCertificateClient.of(virksertUrl, Mode.CODECEPTJS);
-            } else if (properties.getVirksert().getMode().equalsIgnoreCase("test")) {
-                virksertClient = BusinessCertificateClient.of(virksertUrl, Mode.TEST);
-
-              // Is defaulting to MOVE sensible? Easier to set up, but could cause issues if not set/there are typos in prod.
-              // Maybe better to be strict when it comes to typos, and throw exception, forcing user to have an active relationship with the property?
-            } else {
-                virksertClient = BusinessCertificateClient.of(virksertUrl, Mode.MOVE);
-            }
-        } catch (BusinessCertificateException e) {
-            throw new ServiceRegistryException(e);
-        }
-        return virksertClient;
     }
 }
